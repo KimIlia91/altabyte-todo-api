a79da1b00f31d2ffb75726d4eedf43b99c193f24
KimIlia91 <paterfamelias@gmail.com>
2026-01-06 03:36:08 +0600

    feat: add AUTH_URL and OAUTH_CLIENT_ID to settings and example env file for improved OAuth configuration

--- app/core/settings.py
+++ app/core/settings.py
@@ -9,12 +9,12 @@ class Settings(BaseSettings):
 
     TODO_DB_URL: str
 
-    AUTH_URL: str = "https://server-production-5c965.up.railway.app"
+    AUTH_URL: str = ""
+    OAUTH_CLIENT_ID: str = ""
     ISSUER: str = f"{AUTH_URL}/application/o/todo-api/"
     JWKS_URL: str = f"{ISSUER}jwks/"
     AUTHORIZE_URL: str = f"{AUTH_URL}/application/o/authorize/"
     TOKEN_URL: str = f"{AUTH_URL}/application/o/token/"
-    OAUTH_CLIENT_ID: str = "6VYHNJ8Cux0yz723FwNSIKd5WYgOAmkWz9mmZHhp"
     AUTH_ALGORITHMS: list[str] = ["RS256"]
 
     model_config = ConfigDict(
